968d62710959a7955f636eee8bed251f31dbcdd5
karugo <karugo2305@gmail.com>
2023-02-24 12:06:47 +0900

    test app

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,22 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <body>
+        <title>tomoo</title>
+        <h1>h1</h1>
+        <h2>h2</h2>
+        <h3>h3</h3>
+        <h4>h4</h4>
+        <h6>h6</h6>
+        <p>p</p>
+        <a href='https://www.c-sticks.com/'>link</a>
+        <ul>
+          <li>li1</li>
+          <li>li2</li>
+        </ul>
+        <img src='https://d2dcan0armyq93.cloudfront.net/photo/odai/600/d25ff19c230a64ce6ca7434ab453a31f_600.jpg'width="100"height="50"/>
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        {/* <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
@@ -17,9 +29,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
+        </a> */}</body>
   );
 }
 
